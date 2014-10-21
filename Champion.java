8132dcfef46d1de1a31b82a9ede43279d15e0402
baugh70 <zakkyzakkba@gmail.com>
2014-10-21 13:22:20 -0400

    Update Champion.java

--- Champion.java
+++ Champion.java
@@ -1,3 +1,11 @@
 public Champions{
-
+  private String champName;
+  private Spell spell1;
+  private Spell spell2;
+  private Spell spell3;
+  private Spell spell4;
+  
+  public Champions(String champion, Spell s2, Spell s3, Spell s3){
+    
+  }
 }
