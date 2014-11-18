e4a4dac16e2a447a41ee198756687e29e64a92ce
baugh70 <zakkyzakkba@gmail.com>
2014-11-17 20:52:10 -0500

    Update Babe.java

--- Babe.java
+++ Babe.java
@@ -1,7 +1,11 @@
 private HashMap<Player, Boolean> canShoot;
 
 public boolean getAllowed(){
-
+  Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
+    public void run(){
+      // Delayed Code
+    }
+  }, time *20); // Time is equal how ever many seconds
 }
 
 
